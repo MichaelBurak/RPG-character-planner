e3addd26b3835e1d8c42004970e3f0a3e904c27a
Michael Burak <michaelrburak@gmail.com>
2017-06-14 14:55:17 -0400

    beginning Character model

---
+++ db/migrate/20170614185337_create_characters.rb
@@ -0,0 +1,14 @@
+class CreateCharacters < ActiveRecord::Migration[5.0]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :subtle
+      t.integer :powerful
+      t.integer :resistant
+      t.integer :level
+      t.integer :XP
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614185011) do
+ActiveRecord::Schema.define(version: 20170614185337) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "subtle"
+    t.integer  "powerful"
+    t.integer  "resistant"
+    t.integer  "level"
+    t.integer  "XP"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                          null: false
