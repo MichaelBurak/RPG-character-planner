1de015e18ab3c405b75537e67dc3febf5971d82c
Michael Burak <michaelrburak@gmail.com>
2017-06-14 15:32:25 -0400

    scaffold controller Characters added

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,61 @@
+class CharactersController < ApplicationController
+  before_action :set_character, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @characters = Character.all
+  end
+
+  def show
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def edit
+  end
+
+  def create
+    @character = Character.new(character_params)
+
+    respond_to do |format|
+      if @character.save
+        format.html { redirect_to @character, notice: 'Character was successfully created.' }
+        format.json { render :show, status: :created, location: @character }
+      else
+        format.html { render :new }
+        format.json { render json: @character.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @character.update(character_params)
+        format.html { redirect_to @character, notice: 'Character was successfully updated.' }
+        format.json { render :show, status: :ok, location: @character }
+      else
+        format.html { render :edit }
+        format.json { render json: @character.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @character.destroy
+    respond_to do |format|
+      format.html { redirect_to characters_url, notice: 'Character was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_character
+      @character = Character.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def character_params
+      params.fetch(:character, {})
+    end
+end
