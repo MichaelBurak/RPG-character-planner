88df7d7f4312733e16d7a7becb51f49fe2176f54
Michael Burak <michaelrburak@gmail.com>
2017-06-16 15:21:34 -0400

    fixing redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
   root 'welcome#home'
   resources :characters do
     resources :planners, only: %i[create show]
-
   end
 
   get 'characters/:id(.:format)/experience', to: 'characters#experience', as: 'character_experience'
-  resources :planners do
+  resources :planners, only: %i[] do
     resources :events
   end
 
