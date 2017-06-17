d1690dbafae8a96ee029456aaac21e9e655a2231
Michael Burak <michaelrburak@gmail.com>
2017-06-17 11:11:46 -0400

    removed associations between planner and treasure, nested form now works

--- app/models/planner.rb
+++ app/models/planner.rb
@@ -2,5 +2,4 @@ class Planner < ApplicationRecord
   belongs_to :character
   belongs_to :user
   has_many :events
-  has_many :treasures, :through => :events
 end
--- app/models/treasure.rb
+++ app/models/treasure.rb
@@ -1,6 +1,5 @@
 class Treasure < ApplicationRecord
   belongs_to :event
-  belongs_to :planner
   validates :name, uniqueness: true
   validates :gold_value, numericality: true
   validates :name, :gold_value, :kind, presence: true
