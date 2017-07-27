71698e61d1d50476084bfa382e9cac6fa2c9bf0f
Michael Burak <michaelrburak@gmail.com>
2017-07-27 15:56:44 -0400

    included deleting treasures

--- app/controllers/treasures_controller.rb
+++ app/controllers/treasures_controller.rb
@@ -1,24 +1,36 @@
 class TreasuresController < ApplicationController
+  before_action :set_planner_treasure_event
+
   def edit
-    @event = Event.find(params[:event_id])
-    @planner = Planner.find(params[:planner_id])
-    @treasure = Treasure.find(params[:id])
   end
 
   def update
-    @planner = Planner.find(params[:planner_id])
-    @event = Event.find(params[:event_id])
-    @treasure = Treasure.find(params[:id])
     if @treasure.update(treasure_params)
-      redirect_to planner_event_path(@planner, @event)
+      redirect_to planner_event_path(@planner, @event), notice: 'Treasure was successfully updated.'
     else
       render :edit
     end
   end
-end
+
+  def destroy
+    if @treasure.destroy
+      redirect_to planner_event_path(@planner, @event), notice: 'Treasure was successfully destroyed.'
+    else
+      render root_path
+    end
+  end
+
 
 private
 
+  def set_planner_treasure_event
+    @planner = Planner.find(params[:planner_id])
+    @event = Event.find(params[:event_id])
+    @treasure = Treasure.find(params[:id])
+  end
+
   def treasure_params
     params.require(:treasure).permit(%i[id name gold_value kind])
-end
+  end
+
+  end
