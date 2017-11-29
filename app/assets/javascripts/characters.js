bafae72082401b0cd84bc4404cf5f403764b6cf4
Michael Burak <michaelrburak@gmail.com>
2017-11-29 11:43:28 -0500

    more info button functionality implemented, only displays data from first character object however

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -1,5 +1,12 @@
 $(function() {
   $(".js-more").on("click", function() {
-    console.log("clicked");
+    const charID = $(".js-expand").attr("data-id");
+    $.get("/characters/" + charID + "/expand/", function(data) {
+      $(".js-expand").html(
+        `<p> Subtle: ${data["subtle"]}</p>
+         <p> Powerful: ${data["powerful"]} </p>
+         <p> Resistant: ${data["resistant"]}</p>
+         <p> Level: ${data["level"]}</p>`);
+    });
   });
-})
+});
