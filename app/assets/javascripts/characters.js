ce49038c33ad7fc91212c0d29f9d2c5ab70f1980
Michael Burak <michaelrburak@gmail.com>
2017-11-29 12:40:54 -0500

    added previous button on show page

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -45,6 +45,24 @@ $(function() {
   });
 });
 
+$(function() {
+  $(".js-previous").on("click", function() {
+    const nextID = parseInt($(".js-next").attr("data-id")) - 1 ;
+    $.get("/characters/" + nextID + ".json", function(data) {
+      $(".js-character").html(
+        `<p> Character Name: ${data["name"]} </p>
+       <p> Subtle: ${data["subtle"]}</p>
+       <p> Powerful: ${data["powerful"]} </p>
+       <p> Resistant: ${data["resistant"]}</p>
+       <p> Level: ${data["level"]}</p>
+       <p> XP: ${data["xp"]}</p>
+       <p> Created by: ${data["user"]["email"]}</p>`
+      );
+      $(".js-next").attr("data-id", data["id"]);
+    });
+  });
+});
+
 //<p> Character Name: <%= character.name %> </p>
 //<p> Subtle: <%= character.subtle %> </p>
 //<p> Powerful: <%= character.powerful %> </p>
