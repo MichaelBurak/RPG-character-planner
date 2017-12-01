84a3c8b1105374e898633a752038b6a3b736ea0e
Michael Burak <michaelrburak@gmail.com>
2017-12-01 07:49:16 -0500

    refactored and added back in previous and next buttons on character show page functionality

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -15,9 +15,7 @@ $(document).ready(function () {
   $(document).on('click', ".show-character", function(e) {
     e.preventDefault()
     const id = $(this).attr("data-id")
-    debugger
     $('#content').html('')
-    debugger
     $.get(`${window.location.pathname}/${id}.json`, function(data) {
         $("#content").html(`<p> Character Name: ${data["name"]} </p>
            <p> Subtle: ${data["subtle"]}</p>
@@ -25,10 +23,46 @@ $(document).ready(function () {
            <p> Resistant: ${data["resistant"]}</p>
            <p> Level: ${data["level"]}</p>
            <p> XP: ${data["xp"]}</p>
-           <p> Created by: ${data["user"]["email"]}</p>`
+           <p> Created by: ${data["user"]["email"]}</p>
+           <p> <button class= "js-next" data-button-id = "${id}"> Next Character </button>
+           <p> <button class= "js-previous" data-button-id = "${id}"> Previous Character </button>`
         );
       });
-
+      $(document).on("click", ".js-next", function() {
+        const nextID = parseInt($(".js-next").attr("data-button-id")) + 1;
+        $.get("/characters/" + nextID + ".json", function(data) {
+          $("#content").html(
+          `<p> Character Name: ${data["name"]} </p>
+           <p> Subtle: ${data["subtle"]}</p>
+           <p> Powerful: ${data["powerful"]} </p>
+           <p> Resistant: ${data["resistant"]}</p>
+           <p> Level: ${data["level"]}</p>
+           <p> XP: ${data["xp"]}</p>
+           <p> Created by: ${data["user"]["email"]}</p>
+           <p> <button class= "js-next" data-button-id = "${id}"> Next Character </button>
+           <p> <button class= "js-previous" data-button-id = "${id}"> Previous Character </button>`
+          );
+          $(".js-next").attr("data-button-id", data["id"])
+          $(".js-previous").attr("data-button-id", data["id"]);
+        });
+      });
+      $(document).on("click", ".js-previous", function() {
+        const previousID = parseInt($(".js-previous").attr("data-button-id")) - 1;
+        $.get("/characters/" + previousID + ".json", function(data) {
+          $("#content").html(
+          `<p> Character Name: ${data["name"]} </p>
+           <p> Subtle: ${data["subtle"]}</p>
+           <p> Powerful: ${data["powerful"]} </p>
+           <p> Resistant: ${data["resistant"]}</p>
+           <p> Level: ${data["level"]}</p>
+           <p> XP: ${data["xp"]}</p>
+           <p> Created by: ${data["user"]["email"]}</p>
+           <p> <button class= "js-next" data-button-id = "${id}"> Next Character </button>
+           <p> <button class= "js-previous" data-button-id = "${id}"> Previous Character </button>`
+          );
+          $(".js-previous").attr("data-button-id", data["id"]);
+        });
+      })
     });
 
 // $(".characters.show").ready(function () {
@@ -50,21 +84,7 @@ $(document).ready(function () {
 
 // $(".characters.show").ready(function () {
 //   debugger
-//   $(".js-next").on("click", function() {
-//     const nextID = parseInt($(".js-next").attr("data-id")) + 1;
-//     $.get("/characters/" + nextID + ".json", function(data) {
-//       $(".js-character").html(
-//       `<p> Character Name: ${data["name"]} </p>
-//        <p> Subtle: ${data["subtle"]}</p>
-//        <p> Powerful: ${data["powerful"]} </p>
-//        <p> Resistant: ${data["resistant"]}</p>
-//        <p> Level: ${data["level"]}</p>
-//        <p> XP: ${data["xp"]}</p>
-//        <p> Created by: ${data["user"]["email"]}</p>`
-//       );
-//       $(".js-next").attr("data-id", data["id"]);
-//     });
-//   });
+
 // });
 //
 // $(".characters.show").ready(function () {
