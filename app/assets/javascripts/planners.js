25975aef49f6837dd218483be1af6dcbb06d6ba2
Michael Burak <michaelrburak@gmail.com>
2017-12-01 16:07:49 -0500

    working on form for new events in Jquery

--- app/assets/javascripts/planners.js
+++ app/assets/javascripts/planners.js
@@ -9,17 +9,29 @@ $(document).on("click", ".show-planner", function (e) {
       $.each(data.events, function(){
          $("#content").append(`<h3> <a href="/planners/${plannerID}/events/${this.id}"> Event: ${this.name}, XP: ${this.xp} </a> </h3> <br>`)
      })
-  $("#content").append(`<form class=“js-add-event” action=/planners/${plannerID}/events/ accept-charset="UTF-8" method="post">
+  $("#content").append(`<form class="new-event" id="new_event" action=/planners/${plannerID}/events/ accept-charset="UTF-8" method="post">
+    <input name="utf8" type="hidden" value="✓">
     Add Event <label for="event_name">Name</label>
     <input type="text" name="event[name]" id="event_name">
     <label for="event_xp">Xp</label>
     <input type="number" name="event[xp]" id="event_xp">
-
-  <div class="actions">
-    <input type="submit" name="commit" value=“Create Event“ data-disable-with=“Create Event“>
-    <input type="hidden" name="authenticity_token" value=${$('meta[name="csrf-token"]')}>
-  </div>
+    <input type="submit" name="commit" value="Create Event" data-disable-with="Create Event">
+    <input type="hidden" name="authenticity_token" value=${$("meta[name='csrf-token']").attr('content')}>
 </form>
 <a class="show-character" data-id="${characterID}" href=""> Back to character <a>`)
    });
+  //   debugger
+
+  $(document).on('submit', ".new-event", function(e) {
+    debugger
+    e.preventDefault();
+    var values = $(this).serialize;
+    var posting = $.post(`planners/${plannerID}/events`, values);
+    debugger
+    posting.done(function (data){
+    debugger
+    console.log(data)
+    debugger
+  })
+  });
 });
