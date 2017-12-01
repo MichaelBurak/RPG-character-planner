eed1a4bb9ecd7ebd89a17a1e5abbc7c0df1c6ee7
Michael Burak <michaelrburak@gmail.com>
2017-12-01 10:36:27 -0500

    add event link restored to planner show

--- app/assets/javascripts/planners.js
+++ app/assets/javascripts/planners.js
@@ -2,16 +2,14 @@ $(document).on("click", ".show-planner", function (e) {
   e.preventDefault()
   const plannerID = $(this).attr("data-planner-id")
   const characterID = $(this).attr("data-character-id")
-  debugger
   $.get(`/characters/${characterID}` + ".json", function(data){
     $("#content").html(`<h1> Character planner for ${data["name"]} </h1> <br> <h1> Events: </h1>`)
     })
   $.get(`/characters/${characterID}/planners/${plannerID}` + ".json", function(data) {
-    debugger
-
       $.each(data.events, function(){
-         $("#content").append(`<h3> Event: ${this.name}, XP: ${this.xp} </h3> <br>`)
+         $("#content").append(`<h3> <a href="/planners/${plannerID}/events/${this.id}"> Event: ${this.name}, XP: ${this.xp} </a> </h3> <br>`)
      })
+  $("#content").append(`<a href="/planners/${plannerID}/events/new">Add event<a>`)
    });
 });
 
@@ -22,10 +20,9 @@ $(document).on("click", ".show-planner", function (e) {
 // </div>
 //
 // <%= link_to "Add event", new_planner_event_path(@planner) %>
+//planners/:planner_id/events/new(.:format)                          events#new
 // <%= link_to "Back", character_path(@character) %>
 //
 //
-// <%# @events.each do |event| %>
-//       <%#= link_to "#{event.name}", planner_event_path(@planner, event) %></p>
-//       <%# end %> </h2>
 //
+//planner_event GET      /planners/:planner_id/events/:id(.:format)                          events#show
