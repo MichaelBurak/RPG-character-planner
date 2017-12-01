04fe9a4c7c86741fec55043d56969e36df5dea37
Michael Burak <michaelrburak@gmail.com>
2017-12-01 08:12:01 -0500

    planner link now displays planner events dynamically

--- app/assets/javascripts/planners.js
+++ app/assets/javascripts/planners.js
@@ -2,9 +2,26 @@ $(document).on("click", ".show-planner", function (e) {
   e.preventDefault()
   const plannerID = $(this).attr("data-planner-id")
   const characterID = $(this).attr("data-character-id")
-  // $.get(window.location.pathname + ".json", function(data) {
-  //     $.each(data.events, function(){
-  //       $(".js-events").append(`<h1> ${this.name} </h1> <br> <h1> XP: ${this.xp} </h1>`)
-  //   })
-  // });
+  $.get(`${characterID}/planners/${plannerID}` + ".json", function(data) {
+    debugger
+      $("#content").html("")
+      $.each(data.events, function(){
+         $("#content").append(`<h1> ${this.name} </h1> <br> <h1> XP: ${this.xp} </h1>`)
+     })
+   });
 });
+
+//<h1>Character planner for: <%= @character.name %></h1>
+
+// <h2>Events:
+// <div class ="js-events">
+// </div>
+//
+// <%= link_to "Add event", new_planner_event_path(@planner) %>
+// <%= link_to "Back", character_path(@character) %>
+//
+//
+// <%# @events.each do |event| %>
+//       <%#= link_to "#{event.name}", planner_event_path(@planner, event) %></p>
+//       <%# end %> </h2>
+//
