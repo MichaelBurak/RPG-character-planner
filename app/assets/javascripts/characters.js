edaeb2ba5931a9d35c697c7d4a93a18bb9591ad0
Michael Burak <michaelrburak@gmail.com>
2017-12-01 07:31:05 -0500

    large refactoring with cernan including moving towards single page architecture instead of class based targeting, removal of turbolinks, wrapping pages in a div

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -1,4 +1,4 @@
-$(".characters.index").ready(function () {
+$(document).ready(function () {
   $(".js-more").on("click", function() {
     let buttonID = $(this).attr("data-id")
     $.get("/characters/" + buttonID + "/expand/", function(data) {
@@ -11,57 +11,77 @@ $(".characters.index").ready(function () {
       );
     });
   });
-});
 
-$(".characters.show").ready(function () {
-  debugger
-  $.get(window.location.pathname + ".json", function(data) {
-    $(".js-character").html(
-      `<p> Character Name: ${data["name"]} </p>
-       <p> Subtle: ${data["subtle"]}</p>
-       <p> Powerful: ${data["powerful"]} </p>
-       <p> Resistant: ${data["resistant"]}</p>
-       <p> Level: ${data["level"]}</p>
-       <p> XP: ${data["xp"]}</p>
-       <p> Created by: ${data["user"]["email"]}</p>`
-    );
-  });
-});
+  $(document).on('click', ".show-character", function(e) {
+    e.preventDefault()
+    const id = $(this).attr("data-id")
+    debugger
+    $('#content').html('')
+    debugger
+    $.get(`${window.location.pathname}/${id}.json`, function(data) {
+        $("#content").html(`<p> Character Name: ${data["name"]} </p>
+           <p> Subtle: ${data["subtle"]}</p>
+           <p> Powerful: ${data["powerful"]} </p>
+           <p> Resistant: ${data["resistant"]}</p>
+           <p> Level: ${data["level"]}</p>
+           <p> XP: ${data["xp"]}</p>
+           <p> Created by: ${data["user"]["email"]}</p>`
+        );
+      });
 
-$(".characters.show").ready(function () {
-  debugger
-  $(".js-next").on("click", function() {
-    const nextID = parseInt($(".js-next").attr("data-id")) + 1;
-    $.get("/characters/" + nextID + ".json", function(data) {
-      $(".js-character").html(
-      `<p> Character Name: ${data["name"]} </p>
-       <p> Subtle: ${data["subtle"]}</p>
-       <p> Powerful: ${data["powerful"]} </p>
-       <p> Resistant: ${data["resistant"]}</p>
-       <p> Level: ${data["level"]}</p>
-       <p> XP: ${data["xp"]}</p>
-       <p> Created by: ${data["user"]["email"]}</p>`
-      );
-      $(".js-next").attr("data-id", data["id"]);
     });
-  });
-});
 
-$(".characters.show").ready(function () {
-  debugger 
-  $(".js-previous").on("click", function() {
-    const nextID = parseInt($(".js-next").attr("data-id")) - 1 ;
-    $.get("/characters/" + nextID + ".json", function(data) {
-      $(".js-character").html(
-      `<p> Character Name: ${data["name"]} </p>
-       <p> Subtle: ${data["subtle"]}</p>
-       <p> Powerful: ${data["powerful"]} </p>
-       <p> Resistant: ${data["resistant"]}</p>
-       <p> Level: ${data["level"]}</p>
-       <p> XP: ${data["xp"]}</p>
-       <p> Created by: ${data["user"]["email"]}</p>`
-      );
-      $(".js-next").attr("data-id", data["id"]);
-    });
-  });
+// $(".characters.show").ready(function () {
+//   debugger
+//   $.get(window.location.pathname + ".json", function(data) {
+//     $(".js-character").html(
+//       `<p> Character Name: ${data["name"]} </p>
+//        <p> Subtle: ${data["subtle"]}</p>
+//        <p> Powerful: ${data["powerful"]} </p>
+//        <p> Resistant: ${data["resistant"]}</p>
+//        <p> Level: ${data["level"]}</p>
+//        <p> XP: ${data["xp"]}</p>
+//        <p> Created by: ${data["user"]["email"]}</p>`
+//     );
+//   });
+// });
+
+
+
+// $(".characters.show").ready(function () {
+//   debugger
+//   $(".js-next").on("click", function() {
+//     const nextID = parseInt($(".js-next").attr("data-id")) + 1;
+//     $.get("/characters/" + nextID + ".json", function(data) {
+//       $(".js-character").html(
+//       `<p> Character Name: ${data["name"]} </p>
+//        <p> Subtle: ${data["subtle"]}</p>
+//        <p> Powerful: ${data["powerful"]} </p>
+//        <p> Resistant: ${data["resistant"]}</p>
+//        <p> Level: ${data["level"]}</p>
+//        <p> XP: ${data["xp"]}</p>
+//        <p> Created by: ${data["user"]["email"]}</p>`
+//       );
+//       $(".js-next").attr("data-id", data["id"]);
+//     });
+//   });
+// });
+//
+// $(".characters.show").ready(function () {
+//   debugger
+//   $(".js-previous").on("click", function() {
+//     const nextID = parseInt($(".js-next").attr("data-id")) - 1 ;
+//     $.get("/characters/" + nextID + ".json", function(data) {
+//       $(".js-character").html(
+//       `<p> Character Name: ${data["name"]} </p>
+//        <p> Subtle: ${data["subtle"]}</p>
+//        <p> Powerful: ${data["powerful"]} </p>
+//        <p> Resistant: ${data["resistant"]}</p>
+//        <p> Level: ${data["level"]}</p>
+//        <p> XP: ${data["xp"]}</p>
+//        <p> Created by: ${data["user"]["email"]}</p>`
+//       );
+//       $(".js-next").attr("data-id", data["id"]);
+//     });
+//   });
 });
--- app/assets/javascripts/planners.js
+++ app/assets/javascripts/planners.js
@@ -1,8 +1,8 @@
-$(".planners.show").ready(function () {
-  debugger
-  $.get(window.location.pathname + ".json", function(data) {
-      $.each(data.events, function(){
-        $(".js-events").append(`<h1> ${this.name} </h1> <br> <h1> XP: ${this.xp} </h1>`)
-    })
-  });
-});
+// $(".planners.show").ready(function () {
+//   debugger
+//   $.get(window.location.pathname + ".json", function(data) {
+//       $.each(data.events, function(){
+//         $(".js-events").append(`<h1> ${this.name} </h1> <br> <h1> XP: ${this.xp} </h1>`)
+//     })
+//   });
+// });
