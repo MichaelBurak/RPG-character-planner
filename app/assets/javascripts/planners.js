29b0bc042bbfa2d4d1ac1b55610a29d168dededb
Michael Burak <michaelrburak@gmail.com>
2017-12-01 08:05:29 -0500

    preventing default and setting variables for interpolation in get request for clicking on show planner link

--- app/assets/javascripts/planners.js
+++ app/assets/javascripts/planners.js
@@ -1,8 +1,10 @@
-// $(".planners.show").ready(function () {
-//   debugger
-//   $.get(window.location.pathname + ".json", function(data) {
-//       $.each(data.events, function(){
-//         $(".js-events").append(`<h1> ${this.name} </h1> <br> <h1> XP: ${this.xp} </h1>`)
-//     })
-//   });
-// });
+$(document).on("click", ".show-planner", function (e) {
+  e.preventDefault()
+  const plannerID = $(this).attr("data-planner-id")
+  const characterID = $(this).attr("data-character-id")
+  // $.get(window.location.pathname + ".json", function(data) {
+  //     $.each(data.events, function(){
+  //       $(".js-events").append(`<h1> ${this.name} </h1> <br> <h1> XP: ${this.xp} </h1>`)
+  //   })
+  // });
+});
