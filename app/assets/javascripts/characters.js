f4373364ce10a88067b688e2ac52617ea5aae190
Michael Burak <michaelrburak@gmail.com>
2017-12-01 07:57:07 -0500

    adding class and data to links to planner, refactoring planner display jquery

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -60,48 +60,10 @@ $(document).ready(function () {
            <p> <button class= "js-next" data-button-id = "${id}"> Next Character </button>
            <p> <button class= "js-previous" data-button-id = "${id}"> Previous Character </button>`
           );
+          $(".js-next").attr("data-button-id", data["id"])
           $(".js-previous").attr("data-button-id", data["id"]);
         });
       })
     });
 
-// $(".characters.show").ready(function () {
-//   debugger
-//   $.get(window.location.pathname + ".json", function(data) {
-//     $(".js-character").html(
-//       `<p> Character Name: ${data["name"]} </p>
-//        <p> Subtle: ${data["subtle"]}</p>
-//        <p> Powerful: ${data["powerful"]} </p>
-//        <p> Resistant: ${data["resistant"]}</p>
-//        <p> Level: ${data["level"]}</p>
-//        <p> XP: ${data["xp"]}</p>
-//        <p> Created by: ${data["user"]["email"]}</p>`
-//     );
-//   });
-// });
-
-
-
-// $(".characters.show").ready(function () {
-//   debugger
-
-// });
-//
-// $(".characters.show").ready(function () {
-//   debugger
-//   $(".js-previous").on("click", function() {
-//     const nextID = parseInt($(".js-next").attr("data-id")) - 1 ;
-//     $.get("/characters/" + nextID + ".json", function(data) {
-//       $(".js-character").html(
-//       `<p> Character Name: ${data["name"]} </p>
-//        <p> Subtle: ${data["subtle"]}</p>
-//        <p> Powerful: ${data["powerful"]} </p>
-//        <p> Resistant: ${data["resistant"]}</p>
-//        <p> Level: ${data["level"]}</p>
-//        <p> XP: ${data["xp"]}</p>
-//        <p> Created by: ${data["user"]["email"]}</p>`
-//       );
-//       $(".js-next").attr("data-id", data["id"]);
-//     });
-//   });
 });
