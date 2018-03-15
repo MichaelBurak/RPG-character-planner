becd0cb318d944d3163ca7d47fecca7913e621d4
Michael Burak <michaelrburak@gmail.com>
2018-03-15 09:02:44 -0400

    show character click jQuery removed. prev/next now grab accurate id.

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -1,6 +1,6 @@
 //When button with js-more class is clicked, introspect on button's data-id corresponding to character's id
 //and display more info from API route of expand using newly created ECharacter object and prototypal method.
-  $(document).ready(function () {
+  $(function () {
     function ECharacter(subtle, powerful, resistant, level, xp){
       this.subtle = subtle
       this.powerful = powerful
@@ -25,34 +25,14 @@
      });
    })
 
-//When character show page link is clicked, clear html, then get request from json version
-//of character page and populate with JSON versions of ActiveRecord model, with buttons.
-
-$(document).on('click', ".show-character", function(e) {
-  e.preventDefault()
-  const id = $(this).attr("data-id")
-  $('#content').html('')
-  $.get(`/characters/${id}.json`, function(data) {
-      $("#content").html(`<p> Character Name: ${data["name"]} </p>
-         <p> Subtle: ${data["subtle"]}</p>
-         <p> Powerful: ${data["powerful"]} </p>
-         <p> Resistant: ${data["resistant"]}</p>
-         <p> Level: ${data["level"]}</p>
-         <p> XP: ${data["xp"]}</p>
-         <p> Created by: ${data["user"]["email"]}</p>
-         <p> <button class= "js-next" data-button-id = "${id}"> Next Character </button>
-         <p> <button class= "js-previous" data-button-id = "${id}"> Previous Character </button>
-         <p> <button class= "js-delete" data-button-id = "${id}"> Delete Character </button>`
-      );
-    });
-
-
-  //When js-next class button is clicked, set const to button-id + 1 or -1, grab and replace content with
-  //next or previous character. Reset button-ids on previous and next buttons.
+  //When js-next or js-previous class button is clicked, grab and replace content with
+  //next or previous character based on id. Reset button-ids on previous and next buttons.
       $(document).on("click", ".js-next", function() {
-        const nextID = parseInt($(".js-next").attr("data-button-id")) + 1;
-        $.get("/characters/" + nextID + ".json", function(data) {
-          $("#content").html(
+      $.get("/characters.json/", function(data){
+         $.get("/characters/" + `${data.find(function (i){ 
+          return i.id > parseInt($(".js-next").attr("data-button-id"))
+          }).id}` + ".json", function(data) {
+          $(".js-character").html(
           `<p> Character Name: ${data["name"]} </p>
            <p> Subtle: ${data["subtle"]}</p>
            <p> Powerful: ${data["powerful"]} </p>
@@ -60,19 +40,23 @@ $(document).on('click', ".show-character", function(e) {
            <p> Level: ${data["level"]}</p>
            <p> XP: ${data["xp"]}</p>
            <p> Created by: ${data["user"]["email"]}</p>
-           <p> <button class= "js-next" data-button-id = "${id}"> Next Character </button>
-           <p> <button class= "js-previous" data-button-id = "${id}"> Previous Character </button>
-           <p> <button class= "js-delete" data-button-id = "${id}"> Delete Character </button>`
+           <p> <button class= "js-next" data-button-id = "${data["id"]}"> Next Character </button>
+           <p> <button class= "js-previous" data-button-id = "${data["id"]}"> Previous Character </button>
+           <p> <button class= "js-delete" data-button-id = "${data["id"]}"> Delete Character </button>`
           );
           $(".js-next").attr("data-button-id", data["id"])
           $(".js-previous").attr("data-button-id", data["id"]);
           $(".js-delete").attr("data-button-id", data["id"])
         });
       });
+    })
+         })
       $(document).on("click", ".js-previous", function() {
-        const previousID = parseInt($(".js-previous").attr("data-button-id")) - 1;
-        $.get("/characters/" + previousID + ".json", function(data) {
-          $("#content").html(
+        $.get("/characters.json/", function(data){
+         $.get("/characters/" + `${data.reverse().find(function (i){
+          return i.id < parseInt($(".js-previous").attr("data-button-id"))
+          }).id}` + ".json", function(data) {
+          $(".js-character").html(
           `<p> Character Name: ${data["name"]} </p>
            <p> Subtle: ${data["subtle"]}</p>
            <p> Powerful: ${data["powerful"]} </p>
@@ -80,9 +64,9 @@ $(document).on('click', ".show-character", function(e) {
            <p> Level: ${data["level"]}</p>
            <p> XP: ${data["xp"]}</p>
            <p> Created by: ${data["user"]["email"]}</p>
-           <p> <button class= "js-next" data-button-id = "${id}"> Next Character </button>
-           <p> <button class= "js-previous" data-button-id = "${id}"> Previous Character </button>
-           <p> <button class= "js-delete" data-button-id = "${id}"> Delete Character </button>`
+           <p> <button class= "js-next" data-button-id = "${data["id"]}"> Next Character </button>
+           <p> <button class= "js-previous" data-button-id = "${data["id"]}"> Previous Character </button>
+           <p> <button class= "js-delete" data-button-id = "${data["id"]}"> Delete Character </button>`
           );
           $(".js-next").attr("data-button-id", data["id"])
           $(".js-previous").attr("data-button-id", data["id"]);
@@ -101,5 +85,3 @@ $(document).on('click', ".show-character", function(e) {
     }
 });
       });
-
-});
